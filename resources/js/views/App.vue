e59267f1efbb1efb6c560b0a041e63f6340785da
Gumber <Gumber>
2021-03-29 23:43:21 -0400

    compil. files

---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{message}}
+        <example-component></example-component>
+    </div>
+</template>
+
+<script>
+    
+    export default {
+        data: () => ({
+
+            message: 'Test',
+        })
+    };
+
+</script>
